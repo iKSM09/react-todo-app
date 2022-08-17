c61da5f8ba76c9f04e5a2979f52d0e4a5feda474
Kunal Sharma <kunal.iksm09@gmail.com>
2022-08-17 21:30:18 +0530

    created a simple to-do list app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { MdAddCircleOutline, MdRadioButtonUnchecked } from "react-icons/md";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    { id: "sdfb", task: "slfdsidf" },
+    { id: "dbjh", task: "sfkjsdilf" },
+    { id: "ksda", task: "lsjdfiosdhf" },
+    { id: "djkf", task: "wpoejfwe" },
+  ]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Tasks</h1>
+      <section
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <MdAddCircleOutline style={{ fontSize: "22px", margin: "4px" }} />
+        <input
+          type="text"
+          placeholder="Add a Task"
+          onKeyPress={(e) =>
+            e.key === "Enter" &&
+            setTodos([
+              ...todos,
+              { id: new Date().getTime().toString(), task: e.target.value },
+            ])
+          }
+          style={{
+            border: "none",
+            outline: "none",
+            background: "transparent",
+            borderBottom: "1px solid gray",
+            fontSize: "16px",
+          }}
+        />
+      </section>
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          style={{
+            display: "flex",
+            // justifyContent: "center",
+            alignItems: "center",
+            marginBlock: "4px",
+          }}
+        >
+          <MdRadioButtonUnchecked style={{ fontSize: "18px", margin: "6px" }} />
+          <p
+            style={{
+              fontSize: "16px",
+              margin: 0,
+            }}
+          >
+            {todo.task}
+          </p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
